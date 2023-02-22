78ecdc5230aca4b9acb7731ed16e907c5cadfc8f
Sergey Klimanoff <iamjsare@gmail.com>
2023-02-22 09:29:15 -0600

    first commit

---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:coordy_app/app/app.dart';
+import 'package:coordy_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
---
+++ test/app/view/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:coordy_app/app/app.dart';
+import 'package:coordy_app/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
